44a97007a4f9566c9ff3f8387f28390b51f05795
ricardoleitee <ricardo.leitee12@gmail.com>
2014-10-23 19:30:07 +0100

    Update codigo-fonte.pl

--- codigo-fonte.pl
+++ codigo-fonte.pl
@@ -1,4 +1,4 @@
-tabuleiro([
+tabuleiro([9,
 	[0,0,0,1,1,1,0,0,0],
 	[0,0,0,0,1,0,0,0,0],
 	[0,0,1,0,2,2,0,0,0],
@@ -8,6 +8,21 @@ tabuleiro([
 	[0,0,0,0,2,0,2,0,0],
 	[0,2,0,0,0,0,k,0,0],
 	[0,0,0,1,1,1,0,1,0]]).
+
+positions([
+		[00,01,02,03,04,05,06,07,08]]).
+
+printtopline(X,X).
+
+printtopline(X,Y):-
+	write(X),
+	write(' '),
+	Y2 is Y+1,
+	printtopline(X,Y2).
+
+printSboard([X|Y]):-
+	printtopline(X,0),
+	printBoard(Y).
 
 printBoard([]).
 
@@ -30,5 +45,4 @@ printLine([X|Y]):-
 
 game:-
 
-tabuleiro(Board),
-printBoard(Board).
+tabuleiro(Board), printSboard(Board).
